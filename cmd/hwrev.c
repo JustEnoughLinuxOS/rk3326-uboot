a5d77769a7ef58c5ef400db187e601b3ce1b1b8c
Tony Jih <tonyjih@gmail.com>
2021-10-08 19:40:50 -0700

    Separate dtb_name to dtb_uboot & dtb_kernel

--- cmd/hwrev.c
+++ cmd/hwrev.c
@@ -18,32 +18,30 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		printf("board hw rev failed\n");
 		return CMD_RET_FAILURE;
 	}
-#if 1
 	/* RG351MP */
 	if (check_range(146, 186, hwrev_adc)) {
 		env_set("hwrev", "rg351mp");
-		env_set("dtb_name", "rg351mp-uboot.dtb");
+		env_set("dtb_uboot", "rg351mp-uboot.dtb");
+		env_set("dtb_kernel", "rk3326-rg351mp-linux.dtb");
 	}
 	/* RG351V */
 	else if (check_range(494, 534, hwrev_adc)) {
 		env_set("hwrev", "rg351v");
-		env_set("dtb_name", "rg351v-uboot.dtb");
+		env_set("dtb_uboot", "rg351v-uboot.dtb");
+		env_set("dtb_kernel", "rk3326-rg351v-linux.dtb");
 	}
 	/* RG351P */
 	else if (check_range(655, 695, hwrev_adc)) {
 		env_set("hwrev", "rg351p");
-		env_set("dtb_name", "rg351p-uboot.dtb");
+		env_set("dtb_uboot", "rg351p-uboot.dtb");
+		env_set("dtb_kernel", "rk3326-rg351p-linux.dtb");
 	}
 	/* Unknown */
 	else {
 		env_set("hwrev", "v00");
-		env_set("dtb_name", "rg351p-uboot.dtb");
+		env_set("dtb_uboot", "rg351p-uboot.dtb");
+		env_set("dtb_kernel", "rk3326-rg351p-linux.dtb");
 	}
-#else
-		env_set("hwrev", "rg351mp");
-		env_set("dtb_name", "rk3326-rg351mp-linux.dtb");
-
-#endif
 	printf("adc0 (hw rev) %d\n", hwrev_adc);
 printf("Model = %s\n",env_get("hwrev"));
 	return CMD_RET_SUCCESS;
